53306683aafe42b7187081aa83496fe0b7dbc502
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-07-29 09:49:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "24509589"))
-API_HASH = getenv("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75")
+API_ID = int(getenv("API_ID", "23795091"))
+API_HASH = getenv("API_HASH", "64a8dadb2a928e4a97e037e834d1f8db")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7493562717:AAH9DGdNRk5g_kCQ6hxLR39szY-WacI8At0")
-OWNER_ID = int(getenv("OWNER_ID", "7427691214"))
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN", "gitwizardbypass")
+BOT_TOKEN = getenv("BOT_TOKEN", "7283082118:AAEmmoOO3UhxIEXgoVGJV2wiiujuOygFz0k")
+OWNER_ID = int(getenv("OWNER_ID", "7283082118"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://reacode:<password>@reamusic.x7hyx2h.mongodb.net/?retryWrites=true&w=majority&appName=reamusic")
+MUST_JOIN = getenv("MUST_JOIN", "NyxianNetwork")
 DAXX_API = getenv("DAXX_API", "daxx-1490?api+key:free")
